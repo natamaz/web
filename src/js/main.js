eb8e6b8b8322774d93d202cb0d0f826811eb456d
AG <ndrwgldmn@gmail.com>
2021-04-30 11:21:51 +0300

    testimonials

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('.h-testimonials-slider').slick({
+        arrows: false,
+        infinite: true,
+        autoplay: true
+    });
+
+    $('[data-go-to-slide]').click(function() {
+        let targetSlider = $(this).attr('data-target-stider');
+        let targetSlideIndex = $(this).attr('data-go-to-slide');
+        
+        $('[data-go-to-slide]').removeClass('active');
+        $(this).addClass('active');
+
+        $(targetSlider).slick('slickGoTo', targetSlideIndex);
+    });
+
+    $('.h-testimonials-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        console.log(nextSlide);
+        sliderNav = $(this).attr('data-slider-nav');
+        $(sliderNav + ' [data-go-to-slide]').removeClass('active');
+        $(sliderNav + ' [data-go-to-slide="' + nextSlide + '"]').addClass('active');
+    });
+});
---
+++ src/js/vendor.js
@@ -0,0 +1,2 @@
+//=require vendor/jquery/jquery-3.6.0.min.js
+//=require vendor/slick/slick.min.js
